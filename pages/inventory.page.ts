24edb54428c9c7f4902b875e2a8f01d79be4ed8f
MattiusJ <6135322+MattiusJ@users.noreply.github.com>
2025-12-08 08:27:29 +0000

    Add inventory page related tests and page helper

---
+++ pages/inventory.page.ts
@@ -0,0 +1,48 @@
+import { expect, Page } from '@playwright/test';
+import env from '../config/settings';
+
+export const inventory = {
+    inventoryPageUrl: `${env.baseUrl}/inventory.html`,
+    inventoryList: '[data-test="inventory-list"]',
+    inventoryItem: '[data-test="inventory-item"]',
+    inventoryItemImg: '[class="inventory_item_img"]',
+    addToCartButton: '[data-test^="add-to-cart"]', // <-- ^= means starts with...
+    inventoryItemDescription: '[data-test="inventory-item-description"]',
+    inventoryItemPrice: '[data-test="inventory-item-price"]',
+
+    // Sort functionality
+    sortButton: '[data-test="product-sort-container"]',
+
+    items: {
+        item1Title: 'Sauce Labs Backpack',
+        item1Desc: `carry.allTheThings() with the sleek, streamlined Sly Pack that melds uncompromising style with unequaled laptop and tablet protection.`,
+        item1Loc: 'asdf',
+        item1Price: '$29.99',
+        item2Title: 'Sauce Labs Bike Light',
+        item2Desc: `A red light isn't the desired state in testing but it sure helps when riding your bike at night. Water-resistant with 3 lighting modes, 1 AAA battery included.`,
+        item2Price: '$9.99',
+        item2Loc: '',
+    }
+}
+
+export class InventoryPage {
+    readonly page: Page;
+
+    constructor(page: Page) {
+        this.page = page;
+    }
+
+    item(name: string) {
+        return this.page.locator('[data-test="inventory-item"]').filter({
+            hasText: name
+        });
+    }
+
+    async addtoCartByName(name: string) {
+        await this.item(name).locator('button:has-text("Add to cart")').click();
+    }
+
+    async expectPrice(name: string, price: string) {
+        await expect(this.item(name).locator('[data-test="inventory-item-price"]')).toHaveText(price);
+    }
+}
---
+++ tests/inventory-navigation.spec.ts
@@ -0,0 +1,60 @@
+/*
+Just simple tests for showcasing. More complex tests can easily be added if needed, but now we want to keep things simple.
+
+TODO: Fix redundancy with inventoryItems page.locator
+*/
+
+import { test, expect, type Page } from '@playwright/test';
+import { inventory } from '../pages/inventory.page';
+
+
+test.describe('Products inventory page tests', () => {
+    test.beforeEach(async ({ page }) => {
+        await page.goto(inventory.inventoryPageUrl);
+    });
+
+    test('Inventory items are shown', async ({ page }) => {
+        await expect(page).toHaveURL(inventory.inventoryPageUrl);
+
+        await expect(page.getByText('Products')).toBeVisible();
+
+        const inventoryItems = page.locator(inventory.inventoryItem);
+        await expect(inventoryItems).toHaveCount(6);
+    });
+
+    test('Inventory item description is shown', async ({ page }) => {
+        const inventoryItems = page.locator(inventory.inventoryItem);
+        await expect(inventoryItems.first()).toContainText(inventory.items.item1Desc);
+    });
+
+    test('Inventory item prices is shown', async ({ page }) => {
+        const inventoryItems = page.locator(inventory.inventoryItem);
+        await expect(inventoryItems.first()).toContainText(inventory.items.item1Price);
+    });
+
+    test('Inventory item title is shown', async ({ page }) => {
+        const inventoryItems = page.locator(inventory.inventoryItem);
+        await expect(inventoryItems.first()).toContainText(inventory.items.item1Title);
+    });
+
+    test('Inventory images are shown', async ({ page }) => {
+        const inventoryItems = page.locator(inventory.inventoryItem);
+        await expect(inventoryItems.first().locator('img')).toBeVisible();
+        await expect(inventoryItems.first().locator('img')).toHaveCount(1);
+    });
+
+    test('Add to cart button is shown', async ({ page }) => {
+        await expect(page.getByText('Products')).toBeVisible();
+        const inventoryItems = page.locator(inventory.inventoryItem);
+        await expect(inventoryItems.first()).toContainText("Add to cart");
+    });
+
+    //TODO: Here we can start adding more tests to verify the sorting is done correctly and that items shown are in correct order.
+    // Now we just keep it simple.
+    test('Filter button is shown', async ({ page }) => {
+        const sortBtn = page.getByRole('combobox');
+
+        await sortBtn.selectOption('hilo');
+        await expect(page.locator('[data-test="active-option"]')).toHaveText('Price (high to low)');
+    });
+});
